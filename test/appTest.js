c5d00a017834a4ab79e8402b24df9022ac9384fc
unknown <ricardo.j.galdamez@gmail.com>
2020-10-23 11:49:43 -0600

    Adding 24 hours of economy parking test

--- test/appTest.js
+++ test/appTest.js
@@ -311,6 +311,38 @@ describe('Selenium automated functional tests', function(){
             assert.isAtMost(result, 9.00);
             
         });
+        it('Economy parking for 24 hours , cost should be $9 ', async ()=>{
+
+            await driver.findElement(By.name('ParkingLot')).sendKeys('Economy Parking');
+            entryDate = await driver.findElement(By.name('StartingDate'));
+            entryDate.clear();
+            entryDate.sendKeys('10/22/2020');
+            await driver.sleep(200);
+
+            entryTime = await driver.findElement(By.name('StartingTime'));
+            entryTime.clear();
+            entryTime.sendKeys('12:00');
+            await driver.sleep(200);
+
+            leavingDate = await driver.findElement(By.name('LeavingDate'));
+            leavingDate.clear();
+            leavingDate.sendKeys('10/23/2020');
+            await driver.sleep(200);
+
+            leavingTime = await driver.findElement(By.name('LeavingTime'));
+            leavingTime.clear();
+            leavingTime.sendKeys('12:00');
+            await driver.sleep(200);
+
+            await driver.findElement(By.name('Submit')).click();
+            await driver.sleep(500);
+            
+            result = await driver.findElement(By.xpath('//span[@class="SubHead"]/b')).getText();
+            result = result.replace('$','');
+            result = parseFloat(result);
+            assert.equal(result, 9.00);
+            
+        });
         it('Economy parking for 31 days, cost should be $243', async ()=>{
 
             await driver.findElement(By.name('ParkingLot')).sendKeys('Economy Parking');
@@ -408,7 +440,7 @@ describe('Selenium automated functional tests', function(){
         });
     });
     describe('Long-Term Garage Parking tests' , async ()=>{
-
+        
     });
     describe('Long-Term Surface Parking tests', async ()=>{
 
