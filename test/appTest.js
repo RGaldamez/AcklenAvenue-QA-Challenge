af55668f0e8cc172eb6ce9a4ab71703244be981b
unknown <ricardo.j.galdamez@gmail.com>
2020-10-23 11:06:50 -0600

    Adding short term parking test for 5 hours

--- test/appTest.js
+++ test/appTest.js
@@ -21,6 +21,79 @@ describe('Selenium automated functional tests', function(){
         await driver.quit();
     });
 
+    describe('General tampering tests', async ()=>{
+        it('Input letters in the date and time boxes, no PHP warnings should appear', async ()=>{
+
+            await driver.findElement(By.name('ParkingLot')).sendKeys('Valet Parking');
+            entryDate = await driver.findElement(By.name('StartingDate'));
+            entryDate.clear();
+            entryDate.sendKeys('ASDasdjaskduqwjsi');
+            await driver.sleep(200);
+
+            entryTime = await driver.findElement(By.name('StartingTime'));
+            entryTime.clear();
+            entryTime.sendKeys('GASHDHASDGASJDH');
+            await driver.sleep(200);
+
+            leavingDate = await driver.findElement(By.name('LeavingDate'));
+            leavingDate.clear();
+            leavingDate.sendKeys('asdasdasdqwd');
+            await driver.sleep(200);
+
+            leavingTime = await driver.findElement(By.name('LeavingTime'));
+            leavingTime.clear();
+            leavingTime.sendKeys('asdqweq3tesgfsdf');
+            await driver.sleep(200);
+
+            await driver.findElement(By.name('Submit')).click();
+            await driver.sleep(500);
+            
+            result = await driver.findElement(By.xpath('//b')).getText();
+            assert.isUndefined(result, 'php warnings found');  
+        });
+
+        it('Press calculate with default values, user friendly error should show and no php warnings ', async ()=>{
+
+            await driver.findElement(By.name('Submit')).click();
+            await driver.sleep(500);
+            
+            result = await driver.findElement(By.xpath('//b')).getText();
+            assert.isUndefined(result, 'php warnings found');  
+            result = await driver.findElement(By.xpath('//span[@class="SubHead"]/b')).getText();
+            assert.equal(result, 'ERROR! ENTER A CORRECTLY FORMATTED DATE');
+        });
+
+        it('Put a leaving date that happens before the entry date, app should prompt friendly user error', async ()=>{
+
+            await driver.findElement(By.name('ParkingLot')).sendKeys('Valet Parking');
+            entryDate = await driver.findElement(By.name('StartingDate'));
+            entryDate.clear();
+            entryDate.sendKeys('10/30/2020');
+            await driver.sleep(200);
+
+            entryTime = await driver.findElement(By.name('StartingTime'));
+            entryTime.clear();
+            entryTime.sendKeys('12:00');
+            await driver.sleep(200);
+
+            leavingDate = await driver.findElement(By.name('LeavingDate'));
+            leavingDate.clear();
+            leavingDate.sendKeys('10/22/2020');
+            await driver.sleep(200);
+
+            leavingTime = await driver.findElement(By.name('LeavingTime'));
+            leavingTime.clear();
+            leavingTime.sendKeys('12:00');
+            await driver.sleep(200);
+
+            await driver.findElement(By.name('Submit')).click();
+            await driver.sleep(1000);
+            
+            result = await driver.findElement(By.xpath('//td[@class="SubHead"]/b')).getText();
+            assert.equal(result, 'ERROR! YOUR LEAVING DATE OR TIME IS BEFORE YOUR STARTING DATE OR TIME');  
+        });
+    });
+
     describe('Valet Parking Tests', async ()=>{
         it('Selecting Valet parking for 5 hours, cost should be $12', async ()=>{
 
@@ -112,77 +185,42 @@ describe('Selenium automated functional tests', function(){
             assert.equal(result, '$ 54.00');  
         });
 
-        it('Input letters in the date and time boxes, no PHP warnings should appear', async ()=>{
+        
+    });
 
-            await driver.findElement(By.name('ParkingLot')).sendKeys('Valet Parking');
+    describe('Short-Term Parking tests', async ()=>{
+        it('Selecting 1 hour of Short-Term Parking, cost should be $2', async ()=>{
+
+            await driver.findElement(By.name('ParkingLot')).sendKeys('Short-Term Parking');
             entryDate = await driver.findElement(By.name('StartingDate'));
             entryDate.clear();
-            entryDate.sendKeys('ASDasdjaskduqwjsi');
+            entryDate.sendKeys('10/23/2020');
             await driver.sleep(200);
 
             entryTime = await driver.findElement(By.name('StartingTime'));
             entryTime.clear();
-            entryTime.sendKeys('GASHDHASDGASJDH');
+            entryTime.sendKeys('1:00');
             await driver.sleep(200);
 
             leavingDate = await driver.findElement(By.name('LeavingDate'));
             leavingDate.clear();
-            leavingDate.sendKeys('asdasdasdqwd');
+            leavingDate.sendKeys('10/23/2020');
             await driver.sleep(200);
 
             leavingTime = await driver.findElement(By.name('LeavingTime'));
             leavingTime.clear();
-            leavingTime.sendKeys('asdqweq3tesgfsdf');
+            leavingTime.sendKeys('2:00');
             await driver.sleep(200);
 
             await driver.findElement(By.name('Submit')).click();
             await driver.sleep(500);
             
-            result = await driver.findElement(By.xpath('//b')).getText();
-            assert.isUndefined(result, 'php warnings found');  
-        });
-
-        it('Press calculate with default values, user friendly error should show and no php warnings ', async ()=>{
-
-            await driver.findElement(By.name('Submit')).click();
-            await driver.sleep(500);
-            
-            result = await driver.findElement(By.xpath('//b')).getText();
-            assert.isUndefined(result, 'php warnings found');  
             result = await driver.findElement(By.xpath('//span[@class="SubHead"]/b')).getText();
-            assert.equal(result, 'ERROR! ENTER A CORRECTLY FORMATTED DATE');
-        });
-
-        it('Put a leaving date that happens before the entry date, app should prompt friendly user error', async ()=>{
-
-            await driver.findElement(By.name('ParkingLot')).sendKeys('Valet Parking');
-            entryDate = await driver.findElement(By.name('StartingDate'));
-            entryDate.clear();
-            entryDate.sendKeys('10/30/2020');
-            await driver.sleep(200);
-
-            entryTime = await driver.findElement(By.name('StartingTime'));
-            entryTime.clear();
-            entryTime.sendKeys('12:00');
-            await driver.sleep(200);
-
-            leavingDate = await driver.findElement(By.name('LeavingDate'));
-            leavingDate.clear();
-            leavingDate.sendKeys('10/22/2020');
-            await driver.sleep(200);
-
-            leavingTime = await driver.findElement(By.name('LeavingTime'));
-            leavingTime.clear();
-            leavingTime.sendKeys('12:00');
-            await driver.sleep(200);
-
-            await driver.findElement(By.name('Submit')).click();
-            await driver.sleep(1000);
-            
-            result = await driver.findElement(By.xpath('//td[@class="SubHead"]/b')).getText();
-            assert.equal(result, 'ERROR! YOUR LEAVING DATE OR TIME IS BEFORE YOUR STARTING DATE OR TIME');  
+            assert.equal(result, '$ 2.00');  
         });
     });
+
+    
 
 
 });
