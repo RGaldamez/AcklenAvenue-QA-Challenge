e4ade0aed357a48b0424d2489c7c10293cf5aa77
unknown <ricardo.j.galdamez@gmail.com>
2020-10-23 09:51:29 -0600

    Adding one more case, writing first automated test

--- test/appTest.js
+++ test/appTest.js
@@ -1,11 +1,13 @@
 const {Builder, By} = require('selenium-webdriver');
-const assert = require('mocha').assert;
+const assert = require('chai').assert;
 const mainFunction = require('../app').mainFunction;
 
-const URL = 'http://www.shino.de/parkcalc/'
+const URL = 'http://www.shino.de/parkcalc/';
 let driver;
+let entryDate, entryTime,leavingDate,leavingTime;
+let resultContainer, result;
 
-describe('Selenium functional tests', function(){
+describe('Selenium automated functional tests', function(){
     // Unit tests usually last 2000ms so I am increasing the timeout
     // to allow selenium to perform the tests
     this.timeout(40000);
@@ -19,9 +21,36 @@ describe('Selenium functional tests', function(){
         await driver.quit();
     });
 
-    it('Calculate with default values set', async ()=>{
-        await driver.findElement(By.name('Submit')).click();
-        await driver.sleep(2000);
+    describe('Valet Parking Tests', async ()=>{
+        it('Selecting Valet parking for 5 hours, cost should be $12', async ()=>{
+
+            await driver.findElement(By.name('ParkingLot')).sendKeys('Valet Parking');
+            entryDate = await driver.findElement(By.name('StartingDate'));
+            entryDate.clear();
+            entryDate.sendKeys('10/23/2020');
+            await driver.sleep(200);
+
+            entryTime = await driver.findElement(By.name('StartingTime'));
+            entryTime.clear();
+            entryTime.sendKeys('5:00');
+            await driver.sleep(200);
+
+            leavingDate = await driver.findElement(By.name('LeavingDate'));
+            leavingDate.clear();
+            leavingDate.sendKeys('10/23/2020');
+            await driver.sleep(200);
+
+            leavingTime = await driver.findElement(By.name('LeavingTime'));
+            leavingTime.clear();
+            leavingTime.sendKeys('10:00');
+            await driver.sleep(200);
+
+            await driver.findElement(By.name('Submit')).click();
+            await driver.sleep(500);
+            
+            result = await driver.findElement(By.xpath('//span[@class="SubHead"]/b')).getText();
+            assert.equal(result, '$ 12.00');  
+        });
     });
 
 
